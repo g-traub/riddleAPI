9dda1cbbe6a175ab5e9ddfb180d489c6df265135
gtrb <guillaume.trb@hotmail.com>
2019-06-09 15:07:07 +0200

    scraper functionnal

--- scraper.php
+++ scraper.php
@@ -1,1 +1,32 @@
 <?php
+require "vendor/autoload.php";
+use Goutte\Client;
+$client = new Client;
+
+
+$urls = [
+  'https://www.meilleuresdevinettes.com/bizarre/',
+  'https://www.meilleuresdevinettes.com/difficile/',
+  'https://www.meilleuresdevinettes.com/compliquee/',
+  'https://www.meilleuresdevinettes.com/meilleure/'
+];
+
+$riddles = [];
+
+foreach($urls as $url){
+  $crawler = $client->request('GET', $url);
+  
+  $crawler->filter('.news')->each(function ($node) {
+    global $riddles;
+    $question = $node->filter('.news p:nth-of-type(1)')->text();
+    $answer = $node->filter('.news .reponse')->text();
+  
+    $riddles[] = [
+      'question' => $question,
+      'answer' => $answer
+    ];
+  }); 
+}
+
+var_dump($riddles);
+
