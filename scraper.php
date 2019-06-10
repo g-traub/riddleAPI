9c6be008c221c185ec4d68eaf88ad01462845b0b
gtrb <guillaume.trb@hotmail.com>
2019-06-10 16:12:46 +0200

    added categories to data and new pages to scrape

--- scraper.php
+++ scraper.php
@@ -8,20 +8,31 @@ $urls = [
   'https://www.meilleuresdevinettes.com/bizarre/',
   'https://www.meilleuresdevinettes.com/difficile/',
   'https://www.meilleuresdevinettes.com/compliquee/',
-  'https://www.meilleuresdevinettes.com/meilleure/'
+  'https://www.meilleuresdevinettes.com/meilleure/',
+  'https://www.meilleuresdevinettes.com/alphabet/',
+  'https://www.meilleuresdevinettes.com/animaux/',
+  'https://www.meilleuresdevinettes.com/charade/',
+  'https://www.meilleuresdevinettes.com/facile/',
+  'https://www.meilleuresdevinettes.com/localite/',
+  'https://www.meilleuresdevinettes.com/monsieuretmadame/',
+  'https://www.meilleuresdevinettes.com/nourriture/',
+  'https://www.meilleuresdevinettes.com/pays/',
+  'https://www.meilleuresdevinettes.com/sport/'
 ];
 
 $riddles = [];
 
 foreach($urls as $url){
   $crawler = $client->request('GET', $url);
-  
+  $category = $crawler->filter('#mainnews > h2')->text();
+
   $crawler->filter('.news')->each(function ($node) {
-    global $riddles;
+    global $riddles, $category;
     $question = $node->filter('.news p:nth-of-type(1)')->text();
     $answer = $node->filter('.news .reponse')->text();
   
     $riddles[] = [
+      'category' => $category,
       'question' => $question,
       'answer' => $answer
     ];
