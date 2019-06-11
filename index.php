8ebaf9f4ee657b50677f7f13a5b84eaa5b76256d
gtrb <guillaume.trb@hotmail.com>
2019-06-11 22:41:56 +0200

    deleted views, single page

--- index.php
+++ index.php
@@ -1,45 +1,53 @@
-<?php 
-$title = 'Devinette API';
-$categories = ['Alphabet', 'Animaux', 'Bizarre', 'Charade', 'Compliquée', 'Difficile', 'Facile', 'Localité', 'Meilleure', 'Monsieur et madame', 'Nourriture', 'Pays', 'Sport'];
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Devinettes API</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" rel="stylesheet">
+    </head>
 
-ob_start(); ?>
+    <body class="bg-dark">
+      <header class="navbar navbar-dark bg-primary">
+          <h1 class="text-light">Devinettes API</h1>
+      </header>
+      <div class="container">
+
+<?php
+$categories = ['Alphabet', 'Animaux', 'Bizarre', 'Charade', 'Compliquée', 'Difficile', 'Facile', 'Localité', 'Meilleure', 'Monsieur et madame', 'Nourriture', 'Pays', 'Sport'];
+?>
+
 <div class="my-4 fluid-container">
   <div class="
-  
-  <?php 
-  if ((isset($_GET['amount']) && $_GET['amount'] !== '') || (isset($_GET['category']) && $_GET['category'] !== '')){
-    if(isset($_GET['amount']) && $_GET['amount']>=0 && $_GET['amount']< 97)
-    {
-      $amount = $_GET['amount'];
-    } 
-    if(isset($_GET['category']) && in_array($_GET['category'], $categories))
-    {
-      $category = $_GET['category'];
+
+  <?php
+if ((isset($_GET['amount']) && $_GET['amount'] !== '') || (isset($_GET['category']) && $_GET['category'] !== '')) {
+    if (isset($_GET['amount']) && $_GET['amount'] >= 0 && $_GET['amount'] < 97) {
+        $amount = $_GET['amount'];
     }
-  }
-  else {
+    if (isset($_GET['category']) && in_array($_GET['category'], $categories)) {
+        $category = $_GET['category'];
+    }
+} else {
     echo 'd-none';
-  }
-  ?>
+}
+?>
    alert alert-success" role="alert">
     <h3 class="alert-heading">URL d'API généré : </h3>
     <div class="alert alert-light text-success">http://localhost:8888/scrapedDataAPI_PHP/api/api.php<?php
-    if (isset($amount) && isset($category)){
-      echo '?amount=' . $amount . '&category=' . $category;
-    } 
-    else if(isset($amount)){
-      echo '?amount=' . $amount;
-    }
-    else if(isset($category)){
-      echo '?category=' . $category;
-    }
-    ?></div>
+if (isset($amount) && isset($category)) {
+    echo '?amount=' . $amount . '&category=' . $category;
+} else if (isset($amount)) {
+    echo '?amount=' . $amount;
+} else if (isset($category)) {
+    echo '?category=' . $category;
+}
+?></div>
   </div>
 </div>
-<div class="my-4 fluid-container">
+<div class="my-4 py-4 jumbotron fluid-container">
   <h2>Documentation</h2>
   <p>
-    L'url de l'API : 
+    L'url de l'API :
     <!-- URL à réécrire -->
     <div class="alert alert-light">http://localhost:8888/scrapedDataAPI_PHP/api/api.php</div>
   </p>
@@ -56,10 +64,10 @@ ob_start(); ?>
     </p>
     <h5>Liste des catégories : </h5>
     <ul class="list-group list-group-flush">
-      <?php foreach($categories as $category){
-        echo  '<li class="bg-secondary list-group-item">' . $category . '</li>';
-      }
-      ?>
+      <?php foreach ($categories as $category) {
+    echo '<li class="list-group-item">' . $category . '</li>';
+}
+?>
     </ul>
   </div>
   <div class="my-4">
@@ -71,7 +79,7 @@ ob_start(); ?>
     </p>
   </div>
 </div>
-<div class="my-4 jumbotron text-dark">
+<div class="my-4 py-4 jumbotron">
   <h2>Outil d'aide pour l'API</h2>
   <form>
     <div class="form-group">
@@ -83,15 +91,15 @@ ob_start(); ?>
       <label for="category">Catégories</label>
       <select class="form-control custom-select" name="category" id="category">
         <option value="">Sélectionnez une catégorie</option>
-        <?php foreach($categories as $category){
-          echo  '<option value=' . $category . '>' . $category . '</option>';
-        }
-        ?>
+        <?php foreach ($categories as $category) {
+    echo '<option value=' . $category . '>' . $category . '</option>';
+}
+?>
       </select>
     </div>
     <input class="btn btn-primary" type="submit" value="Générer un URL pour l'API">
   </form>
 </div>
-
-<?php $content = ob_get_clean(); ?>
-<?php require('template.php'); ?>
+</div>
+</body>
+</html>
