d56d4329893ffaed663d8592f4a6c0ebb3b35935
gtrb <guillaume.trb@hotmail.com>
2019-06-11 20:21:07 +0200

    added bootstrap and styled page

--- views/ApiHelp.php
+++ views/ApiHelp.php
@@ -1,61 +1,69 @@
-<?php $categories = ['Alphabet', 'Animaux', 'Bizarre', 'Charade', 'Compliquée', 'Difficile', 'Facile', 'Localité', 'Meilleure', 'Monsieur et madame', 'Nourriture', 'Pays', 'Sport'];
+<?php 
+$title = 'Devinette API';
+$categories = ['Alphabet', 'Animaux', 'Bizarre', 'Charade', 'Compliquée', 'Difficile', 'Facile', 'Localité', 'Meilleure', 'Monsieur et madame', 'Nourriture', 'Pays', 'Sport'];
 
 ob_start(); ?>
-
-<h1>API</h1>
-<section>
+<div class="my-4 fluid-container">
+  <div class="d-none alert alert-success" role="alert">
+    <h3 class="alert-heading">URL d'API généré : </h3>
+    <div class="alert alert-light">http://localhost:8888/scrapedDataAPI_PHP/api/api.php</div>
+  </div>
+</div>
+<div class="my-4 fluid-container">
   <h2>Documentation</h2>
   <p>
     L'url de l'API : 
     <!-- URL à réécrire -->
-    <div>http://localhost:8888/scrapedDataAPI_PHP/api/api.php</div>
+    <div class="alert alert-light">http://localhost:8888/scrapedDataAPI_PHP/api/api.php</div>
   </p>
   <h3>Paramètres</h3>
   <p>
     L'url prend deux paramètre : la catégorie et le nombre de devinettes voulues.
   </p>
-  <div>
+  <div class="my-2">
     <h4>Catégorie</h4>
     <p>
       Préciser la catégorie voulue, si aucune catégorie n'est précisée des devinettes de toutes les catégories seront renvoyées.
       exemple :
-      <div>http://localhost:8888/scrapedDataAPI_PHP/api/api.php?category=sport</div>
+      <div class="alert alert-light" >http://localhost:8888/scrapedDataAPI_PHP/api/api.php?category=sport</div>
     </p>
     <h5>Liste des catégories : </h5>
-    <ul>
+    <ul class="list-group list-group-flush">
       <?php foreach($categories as $category){
-        echo  '<li>' . $category . '</li>';
+        echo  '<li class="bg-secondary list-group-item">' . $category . '</li>';
       }
       ?>
     </ul>
   </div>
-  <div>
+  <div class="my-2">
     <h4>Nombre de devinettes</h4>
     <p>
       Préciser le nombre de devinettes voulues, si aucun nombre n'est précisé le maximum de devinettes sera renvoyée.
       exemple :
-      <div>http://localhost:8888/scrapedDataAPI_PHP/api/api.php?amount=25</div>
+      <div class="alert alert-light">http://localhost:8888/scrapedDataAPI_PHP/api/api.php?amount=25</div>
     </p>
   </div>
-</section>
-<section>
+</div>
+<div class="my-4 jumbotron text-dark">
   <h2>Outil d'aide pour l'API</h2>
   <form>
-    <label for="amount">Nombre de devinettes</label>
-    <input type="number" name="amount" id="amount">
-
-    <label for="category">Catégories</label>
-    <select name="category" id="category">
-      <option value="">Sélectionnez une catégorie</option>
-      <?php foreach($categories as $category){
-        echo  '<option value=' . $category . '>' . $category . '</option>';
-      }
-      ?>
-    </select>
-    <input type="submit" value="Générer un url pour l'API">
+    <div class="form-group">
+      <label for="amount">Nombre de devinettes</label>
+      <input class="form-control" type="number" min=0 max=96 name="amount" id="amount">
+    </div>
+    <div class="form-group">
+      <label for="category">Catégories</label>
+      <select class="form-control custom-select" name="category" id="category">
+        <option value="">Sélectionnez une catégorie</option>
+        <?php foreach($categories as $category){
+          echo  '<option value=' . $category . '>' . $category . '</option>';
+        }
+        ?>
+      </select>
+    </div>
+    <input class="btn btn-primary" type="submit" value="Générer un URL pour l'API">
   </form>
-  
-</section>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
--- views/template.php
+++ views/template.php
@@ -3,11 +3,15 @@
     <head>
         <meta charset="utf-8" />
         <title> <?= $title ?></title>
-        <link href="public/css/style.css" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" rel="stylesheet">
     </head>
     
-    <body>
-        <?php include('inc/menu.php')?>
-        <?= $content ?>
+    <body class="bg-dark text-light">
+        <header class="navbar navbar-dark bg-primary">
+            <h1 class="text-light">Devinette API</h1>
+        </header>
+        <div class="container">
+            <?= $content ?>
+        </div>
     </body>
 </html>
